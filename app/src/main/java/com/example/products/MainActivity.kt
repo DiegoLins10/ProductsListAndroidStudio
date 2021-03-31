d7df6748bf4fab8320690b5d5da1f8e7e6678e84
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-03-31 10:18:53 -0300

    lista de produtos android 31/03/21

---
+++ app/src/main/java/com/example/products/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.products
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //criando o adaptador
+
+        val produtoAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1)
+        val listViewProdutos = findViewById<ListView>(R.id.listViewProdutos)
+        val btnInserir = findViewById<Button>(R.id.btnInserir)
+        val txtProduto = findViewById<TextView>(R.id.txtProduto)
+
+        listViewProdutos.adapter = produtoAdapter
+
+        btnInserir.setOnClickListener{
+            val produto = txtProduto.text.toString()
+
+            if(produto.isEmpty()){
+                produtoAdapter.add(produto)
+                txtProduto.text = ""
+            }else{
+                txtProduto.error = "Coloque um produto"
+            }
+            listViewProdutos.setOnItemClickListener{adapterView: AdapterView<*>, view, position : Int, Id: Long->
+                val item = produtoAdapter.getItem(position)
+                produtoAdapter.remove(item)
+            }
+        }
+    }
+}
