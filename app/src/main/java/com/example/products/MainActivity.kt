ef871640c7c472920d687a228cc1229964766147
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-04-07 19:48:20 -0300

    Lista de produtos 2.0 com imagens 07/04/21

--- app/src/main/java/com/example/products/MainActivity.kt
+++ app/src/main/java/com/example/products/MainActivity.kt
@@ -1,36 +1,59 @@
 package com.example.products
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
+
+import java.util.*
+
+import	java.text.NumberFormat
+import	java.util.Locale
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //criando o adaptador
 
-        val produtoAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1)
+        val produtosAdapter = ProdutoAdapter(this)
         val listViewProdutos = findViewById<ListView>(R.id.listViewProdutos)
-        val btnInserir = findViewById<Button>(R.id.btnInserir)
-        val txtProduto = findViewById<TextView>(R.id.txtProduto)
 
-        listViewProdutos.adapter = produtoAdapter
 
-        btnInserir.setOnClickListener{
-            val produto = txtProduto.text.toString()
+        listViewProdutos.adapter = produtosAdapter
 
-            if(produto.isEmpty()){
-                produtoAdapter.add(produto)
-                txtProduto.text = ""
-            }else{
-                txtProduto.error = "Coloque um produto"
-            }
-            listViewProdutos.setOnItemClickListener{adapterView: AdapterView<*>, view, position : Int, Id: Long->
-                val item = produtoAdapter.getItem(position)
-                produtoAdapter.remove(item)
-            }
+        btnAdicionar.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        listViewProdutos.setOnItemClickListener { adapterView: AdapterView<*>, view, position: Int, id ->
+            val item = produtosAdapter.getItem(position)
+            produtosAdapter.remove(item)
+
         }
     }
+    override fun onResume()  {
+        super.onResume()
+
+        val adapter = listViewProdutos.adapter as ProdutoAdapter
+
+        adapter.clear()
+        adapter.addAll(produtosGlobal)
+
+        var soma = 0.0
+
+        for(item in produtosGlobal){
+            soma += item.valor * item.quantidade
+        }
+        val f = NumberFormat.getCurrencyInstance(Locale("pt", "b"))
+        txtTotal.text = "Total: R$ ${f.format(soma)}"
+    }
+
 }
